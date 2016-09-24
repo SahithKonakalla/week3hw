b351e800045021dd89a9dfb4f7129dba5fdbb4dd
Option16 <sahithk1@gmail.com>
2016-09-24 11:45:03 -0700

    2Sahith

--- ExampleProject/main.cpp
+++ ExampleProject/main.cpp
@@ -33,6 +33,21 @@ int main(int argc, char *argv[]){
 	cout << endl << endl;
 	Parser p;
 	cout<<"Cat Command: " << p.getCommandRef("cat") << endl;
-
+	cout<<"nano Command: " << p.getCommandRef("nano") << endl;
+	cout<<"ls Command: " << p.getCommandRef("ls") << endl;
+	cout<<"rm Command: " << p.getCommandRef("rm") << endl;
+	cout<<"cd Command: " << p.getCommandRef("cd") << endl;
+	if (argv[1] == "nano") {
+		return "A terminal text editor";
+	}
+	if (argv[1] == "ls") {
+		return "Lists everything in the current directory";
+	}
+	if (argv[1] == "cd") {
+		return "Change directory";
+	}
+	if (argv[1] == "rm") {
+		return "Remove file";
+	}
 	return 0;
 }
